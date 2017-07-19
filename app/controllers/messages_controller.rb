f9875cbf1c59298711a0eab10861da62c562f154
mharribey <mathieuharribey@hotmail.fr>
2017-07-19 11:08:33 +0200

    almost finish adding images

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,7 @@ class MessagesController < ApplicationController
                             channel: @channel,
                             content: message_params[:content]
                           )
+
     if @message.save
       ActionCable.server.broadcast "room_channel_#{@channel.id}",
                                     channel_id: @channel.id,
@@ -13,15 +14,18 @@ class MessagesController < ApplicationController
                                     content: @message.content,
                                     msg_date: @message.updated_at.strftime("%d/%m/%y à %Hh%M"),
                                     alias: current_user.alias,
-                                    user_id: current_user.id
+                                    user_id: current_user.id,
+                                    image_link: Rinku.auto_link(@message.content, :all, 'target="_blank"').html_safe,
+                                    image_preview: @iframely.get_oembed_json(@channel.messages.first_url)["html"].html_safe if @iframely.get_oembed_json(@channel.messages.first_url)["html"]
 
       ActionCable.server.broadcast "notification_channel",
                                     content: @message.content,
                                     channel_id: @channel.id,
                                     last_message_user_id: Message.last.user.id
 
-      end
+    end
   end
+
   #     redirect_to @channel
   #   else
   #     @messages = @channel.messages
